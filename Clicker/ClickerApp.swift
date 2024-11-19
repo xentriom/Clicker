fc674cad4912b111a904a38a8a5dd2fe636cbc0b
Jason <c04jason@gmail.com>
2024-11-18 21:10:04 -0500

    Initial Commit

---
+++ Clicker/ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickerApp.swift
+//  Clicker
+//
+//  Created by Jason Chen on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
